a634d6e8eb2c87cefc01cbeb0d20d4c9603b14a7
judas <churoo0418@gmail.com>
2023-07-17 11:29:07 +0900

    Initial Commit

---
+++ calculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by t2023-m0081 on 2023/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by t2023-m0081 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
